2e9157102ab40463d3b03ae657ccb3e752d59f65
Prakash709167 <prakash709167@gmail.com>
2023-12-19 21:29:25 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,11 @@
+const accountId = 254654;
+let accountNumber = "658978468856";
+var accountName = "prakash";
+accountCity = "jaipur";
+let accountState;
+
+accountNumber = "9632587412365";
+accountName = "Aman";
+accountCity = "Bihar";
+
+console.table([accountId, accountName, accountNumber, accountCity,accountState]);
